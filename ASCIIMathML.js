1a208bd0f3b62c9e2fc9ecb30654c50962d3b940
David Lippman <drlippman@yahoo.com>
2014-08-16 23:04:34 -0700

    Avoid javascript errors in AMremovebrackets from childless nodes. Bug and solution via lwz on Google Groups. Fixes #24.

--- ASCIIMathML.js
+++ ASCIIMathML.js
@@ -563,6 +563,7 @@ function AMgetSymbol(str) {
 
 function AMremoveBrackets(node) {
   var st;
+  if (!node.hasChildNodes()) { return; }
   if (node.firstChild.hasChildNodes() && (node.nodeName=="mrow" || node.nodeName=="M:MROW")) {
     st = node.firstChild.firstChild.nodeValue;
     if (st=="(" || st=="[" || st=="{") node.removeChild(node.firstChild);
